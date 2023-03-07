902f4d8c57be04d8f25523ec9338608c9d7a09b1
unknown <luismatthews29@gmail.com>
2023-03-07 13:06:56 -0300

    add assets

--- src/syles/themes/default.ts
+++ src/syles/themes/default.ts
@@ -2,8 +2,25 @@ export const defaultTheme = {
 
    // propriedades
 
+   // products
    darkyellow: '#c47f17',
    yellow:'#DBAC2C',
-   yellowlight:''
+   yellowlight:'#f1E9C9',
+   purpleDark:'#4B2995',
+   purple:'#8047f8',
+   purpleLight:'#EBE5F9',
+
+   // Base
+
+   baseTitle :'#272221',
+   baseSubtitle :'#403937',
+   baseText: '#574f4d',
+   baseLabel:'#8D8686',
+   baseHover:'#D7D5D5',
+   baseButton:'#E6E5E5',
+   baseInput:'#EDEDED',
+   baseCard: '#F3F2F2',
+   background:'#fafafa',
+   white: '#FFFFFF', 
 
 }
